907ebcfa8f6b6e6f08569615f3c6859e6c3acf02
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-22 03:18:52 +0000

    Add seed Users and products data into Mongoose using POSTMAN

--- api/databaseSeeder.js
+++ api/databaseSeeder.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const User = require('./models/User');
 const users = require('./data/Users');
-
+const products = require('./data/Products');
+const Product = require('./models/product');
 router.post('/users', async(req,res)=>{
     await User.deleteMany({});
     const UserSeeder = await User.insertMany(users);
     res.send({UserSeeder});
+
+});
+router.post('/products', async(req,res)=>{
+    await Product.deleteMany({});
+    const ProductSeeder = await Product.insertMany(products);
+    res.send({ProductSeeder});
 
 });
 
