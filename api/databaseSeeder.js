c1fd075131a3adfe3be2cf414cecbad47435bc50
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-23 16:02:33 +0000

    Add Express Async Error Handler

--- api/databaseSeeder.js
+++ api/databaseSeeder.js
@@ -3,21 +3,22 @@ const User = require('./models/User');
 const users = require('./data/Users');
 const products = require('./data/Products');
 const Product = require('./models/product');
+const AsyncHandler = require('express-async-handler');
 
 //Users Seed
-router.post('/users', async(req,res)=>{
+router.post('/users', AsyncHandler(async(req,res)=>{
     await User.deleteMany({});
     const UserSeeder = await User.insertMany(users);
     res.send({UserSeeder});
 
-});
+}) );
 
 //Products Seed
-router.post('/products', async(req,res)=>{
+router.post('/products', AsyncHandler(async(req,res)=>{
     await Product.deleteMany({});
     const ProductSeeder = await Product.insertMany(products);
     res.send({ProductSeeder});
 
-});
+}));
 
 module.exports = router;
