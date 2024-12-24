0adc2dbd89d133a18a52cc0faffe47f35bbfb1c9
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-24 16:43:06 +0000

    Add custom method to encrypt register password

--- api/models/User.js
+++ api/models/User.js
@@ -18,4 +18,13 @@ userSchema.methods.matchPassword = async function (enterPassword) {
     
 }
 
+//register password hash and store
+userSchema.pre("save", async function(next) {
+if(!this.isModified('password')) {
+    next();
+}
+const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt)
+})
+
 module.exports = mongoose.model('User', userSchema);
