4cba3c666ce798e5b021aabe5fb0b4670b66d394
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-25 18:45:01 +0000

    Update Log statement for debug order route

--- api/routes/Order.js
+++ api/routes/Order.js
@@ -36,4 +36,23 @@ orderRoute.post('/',protect, asyncHandler(async(req ,res)=>{
   }
 ));
 
+orderRoute.put('/:id/payment', protect, asyncHandler(async(req, res)=>{
+    const order = await Order.findById(req.params.id);
+    console.log(order);
+    if(order){
+        order.isPaid = true
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address
+        }
+        const updateOrder = order.save();
+        res.json(updateOrder);
+    }else{
+        res.status(404);
+        throw new Error('Order not found');
+    }
+}))
 module.exports = orderRoute;
