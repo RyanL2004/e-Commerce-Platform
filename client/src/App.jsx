ab3674b418a7392bd85345833837e28a3cf7789f
RyanL2004 <rayanlouahche2004@gmail.com>
2024-12-31 23:43:23 +0000

    Add Login/Register Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from './pages/ProductDetail';
 import Home from './pages/Home';
+import { Login } from "./pages/Auth/Login";
+import { Register } from "./pages/Auth/Register";
 function App() {
   return (
     <>
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}>{console.log(App)}</Route>
           <Route exact path="/products/:id" element={<ProductDetail />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
         </Routes>
       </Router>
 
