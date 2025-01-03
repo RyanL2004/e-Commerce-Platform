3000eb72ba101d9a65684fac3fa47bc35d70cf7a
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-03 02:45:21 +0000

    Update Register/Login Paths for User already logged or registered

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,24 @@ import { useState } from "react";
 import Layout from "./Layouts/Layouts";
 import Products from "./components/Products";
 */
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ProductDetail from './pages/ProductDetail';
 import Home from './pages/Home';
 import { Login } from "./pages/Auth/Login";
 import { Register } from "./pages/Auth/Register";
+import { useSelector } from "react-redux";
 function App() {
+  const userLoginReducer = useSelector((state) => state.userLoginReducer)
+  const { userInfo } = userLoginReducer
+
   return (
     <>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}>{console.log(App)}</Route>
           <Route exact path="/products/:id" element={<ProductDetail />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/login" element={userInfo ? <Navigate to="/"/> : <Login /> }></Route>
+          <Route exact path="/register" element={userInfo? <Navigate to="/"/> : <Register />}></Route>
         </Routes>
       </Router>
 
