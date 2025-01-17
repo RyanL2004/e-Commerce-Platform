097c23f7efe29a1656816c40db8e850c3f1507b0
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-17 00:55:04 +0000

    Refactor: Dropdown Section into User Section

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,10 +1,14 @@
 "use client";
-
 import { Dropdown } from "flowbite-react";
+import { useSelector } from "react-redux";
+
+
+const userLoginReducer = useSelector((state) => state.userLoginReducer);
+const { loading, error, userInfo } = userLoginReducer;
 
 export function UserDropDown ({ logoutHandler }) {
   return (
-    <Dropdown label="Dropdown button" dismissOnClick={false}>
+    <Dropdown label={userInfo.name} dismissOnClick={false}>
       <Dropdown.Item>Profile</Dropdown.Item>
       <Dropdown.Item>My orders</Dropdown.Item>
       <Dropdown.Item>Settings</Dropdown.Item>
