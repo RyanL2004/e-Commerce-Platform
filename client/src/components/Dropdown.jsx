424bb98a4a0ea08056ce3ffa9129d9fba97375a9
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-17 00:57:47 +0000

    Fix: User Dropdown

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -3,12 +3,10 @@ import { Dropdown } from "flowbite-react";
 import { useSelector } from "react-redux";
 
 
-const userLoginReducer = useSelector((state) => state.userLoginReducer);
-const { loading, error, userInfo } = userLoginReducer;
 
 export function UserDropDown ({ logoutHandler }) {
   return (
-    <Dropdown label={userInfo.name} dismissOnClick={false}>
+    <Dropdown label="User" dismissOnClick={false}>
       <Dropdown.Item>Profile</Dropdown.Item>
       <Dropdown.Item>My orders</Dropdown.Item>
       <Dropdown.Item>Settings</Dropdown.Item>
