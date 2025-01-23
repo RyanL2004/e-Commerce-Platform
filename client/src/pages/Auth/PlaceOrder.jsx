387bbc8bbe9a5d4adc7580d26bd51a698ac67b19
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-23 17:47:00 +0000

    feat: Paypal payment method & Address form

--- client/src/pages/Auth/PlaceOrder.jsx
+++ client/src/pages/Auth/PlaceOrder.jsx
@@ -1,13 +1,37 @@
 import Layout from "../../Layouts/Layouts";
-import { useSelector } from "react-redux";  
+import { useSelector } from "react-redux";
 import CartItem from "../Auth/CartItems";
+import { useState } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 export default function PlaceOrder() {
-  const cart = useSelector((state)=> state.cartReducer)
-  const { cartItems } = cart;
+  const cart = useSelector((state) => state.cartReducer);
+  const { cartItems, shippingAddress } = cart;
+
+  //subtotal without tax + shipping fee
+  const addDecimal = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+  //subtotal price
+  const subtotal = addDecimal(
+    cartItems.reduce((total, item) => total + item.qty * item.price, 0)
+  );
+  const taxPrice = addDecimal(Number(0.1 * subtotal).toFixed(2));
+  const shippingPrice = addDecimal(subtotal > 100 ? 0 : 9.99);
+
+  // total
+  const total = (
+    Number(subtotal) +
+    Number(taxPrice) +
+    Number(shippingPrice)
+  ).toFixed(2);
+
+  //Shipping adress form
+  const [country, setCountry] = useState(shippingAddress.country);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [postCode, setPostCode] = useState(shippingAddress.postCode);
 
   return (
-
-    
     <>
       <Layout>
         <section className="text-gray-600 body-font overflow-hidden">
@@ -15,51 +39,109 @@ export default function PlaceOrder() {
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
               <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                  Order Summary 
+                  Order Summary
                 </h2>
 
                 <p className="leading-relaxed mb-4">
-                  <CartItem cartItems = {cartItems}> </CartItem>
+                  <CartItem cartItems={cartItems}> </CartItem>
                 </p>
-                
+
                 <div className="flex border-t border-gray-200 py-2">
-                  <span className="text-gray-500">Color</span>
-                  <span className="ml-auto text-gray-900">Blue</span>
+                  <span className="text-gray-500">Subtotal</span>
+                  <span className="ml-auto text-gray-900">${subtotal}</span>
                 </div>
                 <div className="flex border-t border-gray-200 py-2">
-                  <span className="text-gray-500">Size</span>
-                  <span className="ml-auto text-gray-900">Medium</span>
+                  <span className="text-gray-500">Tax</span>
+                  <span className="ml-auto text-gray-900">${taxPrice}</span>
                 </div>
                 <div className="flex border-t border-b mb-6 border-gray-200 py-2">
-                  <span className="text-gray-500">Quantity</span>
-                  <span className="ml-auto text-gray-900">4</span>
+                  <span className="text-gray-500">Shipping Price</span>
+                  <span className="ml-auto text-gray-900">
+                    ${shippingPrice}
+                  </span>
                 </div>
                 <div className="flex">
                   <span className="title-font font-medium text-2xl text-gray-900">
-                    $58.00
+                    ${total}
                   </span>
-                  <button className="flex ml-auto text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded">
-                    Button
-                  </button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg
-                      fill="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
-                  </button>
+                  
                 </div>
               </div>
-              <img
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://dummyimage.com/400x400"
-              />
+              <div className="lg:w-1/3 md:w-1/2 g p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+                <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+                  Shipping Address
+                </h2>
+                <div className="relative mb-4">
+                  <label
+                    htmlfor="text"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Country
+                  </label>
+                  <input
+                    type="text"
+                    id="country"
+                    name="country"
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  ></input>
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlfor="test"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    id="city"
+                    name="city"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  ></input>
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlfor="address"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  ></input>
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlfor="text"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Post Code
+                  </label>
+                  <input
+                    type="text"
+                    id="postcode"
+                    name="postcode"
+                    value={postCode}
+                    onChange={(e) => setPostCode(e.target.value)}
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  ></input>
+                </div>
+                <PayPalScriptProvider options={{ clientId: "test" }}>
+                    <PayPalButtons
+                    //createOrder={createOrder}
+                    //onApprove={onApprove}
+                    />
+                  </PayPalScriptProvider>
+              </div>
             </div>
           </div>
         </section>
