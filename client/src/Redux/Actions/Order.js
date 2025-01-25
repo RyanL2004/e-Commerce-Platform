1cd9ba9f2e7df639195b5dd49ebcf4a4b8c7cd5f
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-25 00:53:34 +0000

    Feat: Order Actions

--- client/src/Redux/Actions/Order.js
+++ client/src/Redux/Actions/Order.js
@@ -1,8 +1,7 @@
 import {
     ORDER_REQ,
-    ORDER_RESET,
     ORDER_SUCCESS,
-    ORDER_FAIL,
+
 
     ORDER_PAYMENT_REQ,
     ORDER_PAYMENT_REQ_SUCCESS,
@@ -18,12 +17,133 @@ import {
 } from '../Constants/Order';
 import { BASE_URL } from '../Constants/BASE_URL';
 
-export const orderAction = (order) => async (dispatchEvent, getState) => {
+import { CLEAR_CART } from "../Constants/Cart/"
+import { userLogoutAction } from './User';
+import axios from 'axios';
+
+//order 
+export const orderAction = (order) => async (dispatch, getState) => {
     try{
         dispatch({ type: ORDER_REQ})
         const userInfo = getState().userLoginReducer.userInfo;
-        
+        const config = {
+            headers : {
+                "Content-Type" : "application/json",
+                Authorization : `Bearer ${userInfo.token}`,
+
+
+            }
+        }
+        const { data } = await axios.post (
+            `${BASE_URL}/api/orders`,
+            order,
+            config
+        );
+
+        dispatch({ type: ORDER_SUCCESS, payload: data});
+        dispatch({ type: CLEAR_CART, payload: data});
+
     } catch (error) {
         console.log(error);
     }
+}
+
+// order detail
+export const orderDetailAction = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DETAIL_REQ})
+        const userInfo = getState().userLoginReducer. userInfo;
+        const config = {
+            headers : {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`,
+            }
+        };
+        const { data } = await axios.get(
+            `${BASE_URL}/api/orders/${id}`,
+            config
+        );
+        dispatch({ type: ORDER_DETAIL_REQ_SUCCESS, payload: data});
+
+    }
+    catch (error){
+        const message = error.reponse && error.response.data.message
+        ? error.response.data.message 
+        : error.message;
+        if (message === "Not authorized!") {
+            dispatch(userLogoutAction());
+        }
+        dispatch({
+            type: ORDER_DETAIL_REQ_FAIL,
+            payload: message,
+        });
+    }
+}
+
+
+
+
+//order payment 
+
+export const orderPaymentAction = (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_PAYMENT_REQ});
+        const userInfo = getState().userLoginReducer.userInfo;
+        const config = {
+                headers: {
+                    "Content-Type" : "application/json",
+                    Authorization: `Bearer ${userInfo.token}`,
+                }
+        };
+        const {data} = await axios.put(
+            `${BASE_URL}/api/orders/${orderId}/payment`,
+            paymentResult,
+            config
+        );
+        dispatch({ type: ORDER_PAYMENT_REQ_SUCCESS, payload: data});
+        dispatch(orderDetailAction(orderId))
+        
+    }
+    catch (error) {
+        const message = error.reponse && error.response.data.message
+        ? error.response.data.message 
+        : error.message;
+        
+        if (message === "Not authorized!") {
+            dispatch(userLogoutAction());
+        }
+        dispatch({
+            type: ORDER_PAYMENT_REQ_FAIL,
+            payload: message,
+        });
+    }
+}
+
+export const orderListAction = () => async (dispatch, getState) => {
+    try {
+        dispatch({ ORDER_LIST_REQ});
+        const userInfo = getState().userLoginReducer.userInfo;
+
+        const config = {
+            headers : {
+                Authorization : `Bearer ${userInfo.token}`,
+            },
+        };
+        const { data } = await axios.get(
+            `${BASE_URL}/api/orders`,
+            config
+        )
+        dispatch({ ORDER_LIST_REQ_SUCCESS, payload: data});
+
+    } catch (error) {
+        const message = error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+        if( message === "Not authorized!"){
+            dispatch(userLogoutAction());
+
+        }
+        dispatch({ ORDER_LIST_REQ_FAIL, payload: message});
+    }
+
 }
