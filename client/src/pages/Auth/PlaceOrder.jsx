4024598a5d1d9f9e757bc777896b446dc39c5cb9
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-25 00:56:36 +0000

    Feat: Dispatch payment processing and Save shipping address action

--- client/src/pages/Auth/PlaceOrder.jsx
+++ client/src/pages/Auth/PlaceOrder.jsx
@@ -1,9 +1,15 @@
 import Layout from "../../Layouts/Layouts";
 import { useSelector } from "react-redux";
 import CartItem from "../Auth/CartItems";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { BASE_URL } from "../../Redux/Constants/BASE_URL";
+import { useDispatch } from "react-redux";
+import { orderAction } from "../../Redux/Actions/Order";
+import axios from "axios";
+import { saveShippingAddressAction } from "../../Redux/Actions/Cart";
+
+
 export default function PlaceOrder() {
   const cart = useSelector((state) => state.cartReducer);
   const { cartItems, shippingAddress } = cart;
@@ -33,18 +39,50 @@ export default function PlaceOrder() {
   const [postCode, setPostCode] = useState(shippingAddress.postCode);
 
   const [clientId, setClientId] = useState(null);
+  const [paymentResult, setPaymentResult] = useState({});
 
-
-  const getPaypalClientId = async () =>{
+  const getPaypalClientId = async () => {
     const res = await axios.get(`${BASE_URL}/api/config/paypal`);
     const fetchedClientId = res.data;
     setClientId(fetchedClientId);
-  }
+  };
 
-  useEffect(()=> {
-    getPaypalClientId()
-  }
-  )
+  useEffect(() => {
+    getPaypalClientId();
+  });
+
+  const dispatch = useDispatch();
+
+  const successPaymentHandler = async (paymentResult) => {
+    try {
+      setPaymentResult(paymentResult);
+      dispatch(
+        orderAction({
+          orderItems: cart.cartItems,
+          shippingAddress: cart.shippingAddress,
+          totalPrice: total,
+          paymentMethod: "paypal",
+          price: subtotal,
+          taxPrice: taxPrice,
+          shippingPrice: shippingPrice,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const saveShippingAddress = () => {
+    dispatch(
+      saveShippingAddressAction({
+        address,
+        city,
+        postCode,
+        country,
+      })
+    );
+  };
+  
 
   return (
     <>
@@ -57,9 +95,9 @@ export default function PlaceOrder() {
                   Order Summary
                 </h2>
 
-                <p className="leading-relaxed mb-4">
+                <div className="leading-relaxed mb-4">
                   <CartItem cartItems={cartItems}> </CartItem>
-                </p>
+                </div>
 
                 <div className="flex border-t border-gray-200 py-2">
                   <span className="text-gray-500">Subtotal</span>
@@ -79,7 +117,6 @@ export default function PlaceOrder() {
                   <span className="title-font font-medium text-2xl text-gray-900">
                     ${total}
                   </span>
-                  
                 </div>
               </div>
               <div className="lg:w-1/3 md:w-1/2 g p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
@@ -88,7 +125,7 @@ export default function PlaceOrder() {
                 </h2>
                 <div className="relative mb-4">
                   <label
-                    htmlfor="text"
+                    htmlFor="text"
                     className="leading-7 text-sm text-gray-600"
                   >
                     Country
@@ -104,7 +141,7 @@ export default function PlaceOrder() {
                 </div>
                 <div className="relative mb-4">
                   <label
-                    htmlfor="test"
+                    htmlFor="test"
                     className="leading-7 text-sm text-gray-600"
                   >
                     City
@@ -120,7 +157,7 @@ export default function PlaceOrder() {
                 </div>
                 <div className="relative mb-4">
                   <label
-                    htmlfor="address"
+                    htmlFor="address"
                     className="leading-7 text-sm text-gray-600"
                   >
                     Address
@@ -136,7 +173,7 @@ export default function PlaceOrder() {
                 </div>
                 <div className="relative mb-4">
                   <label
-                    htmlfor="text"
+                    htmlFor="text"
                     className="leading-7 text-sm text-gray-600"
                   >
                     Post Code
@@ -150,13 +187,36 @@ export default function PlaceOrder() {
                     className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   ></input>
                 </div>
-                {cliendId &&(
-                  <PayPalScriptProvider options={{ clientId: "test" }}>
-                  <PayPalButtons
-                  //createOrder={createOrder}
-                  //onApprove={onApprove}
-                  />
-                </PayPalScriptProvider>
+
+                <button 
+                onClick={saveShippingAddress}
+                className=" mb-10 bg-blue-500 text-white py-2 px-4 rounded hover-bg-blue-600 transition duration-200"
+                >
+                  Save Address
+                </button>
+                
+                {clientId && (
+                  <PayPalScriptProvider options={{ clientId: clientId }}>
+                    <PayPalButtons
+                      createOrder={(data, actions) => {
+                        return actions.order.create({
+                          purchase_units: [
+                            {
+                              amount: {
+                                currency_code: "USD",
+                                value: total,
+                              },
+                            },
+                          ],
+                        });
+                      }}
+                      onApprove={(data, actions) => {    
+                        return actions.order.capture().then(function (details) {
+                          successPaymentHandler(details);
+                        })
+                      }}
+                    />
+                  </PayPalScriptProvider>
                 )}
               </div>
             </div>
