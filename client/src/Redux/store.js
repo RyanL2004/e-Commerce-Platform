ae07c38c3df506b64aa1b52870a40505dd60b668
RyanL2004 <rayanlouahche2004@gmail.com>
2025-01-25 00:55:19 +0000

    Add Order Reducers to the Reduc Store

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,44 +1,55 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { productListReducer, productReducer } from './Reducers/Product';
-import { thunk } from 'redux-thunk';
-import { userLoginReducer, userRegisterReducer } from './Reducers/User';
-import { cartReducer } from './Reducers/Cart';
+import { productListReducer, productReducer } from "./Reducers/Product";
+import { thunk } from "redux-thunk";
+import { userLoginReducer, userRegisterReducer } from "./Reducers/User";
+import { cartReducer } from "./Reducers/Cart";
+import {
+  orderCreateReducer,
+  orderDetailReducer,
+  orderPaymentReducer,
+  orderListReducer,
+} from "./Reducers/Order";
 
 // Persist config
 const persistConfig = {
-    key: 'root',
-    storage,
-    version: '1',
-    whitelist: ['cartReducer', 'userLoginReducer'] // Only persist cart and user login state
+  key: "root",
+  storage,
+  version: "1",
+  whitelist: ["cartReducer", "userLoginReducer"], // Only persist cart and user login state
 };
 
 // Initial state for the entire store
 const initialState = {
-    cartReducer: {
-        cartItems: [],
-        shippingAddress: {},
-    },
-    userLoginReducer: {
-        userInfo: null
-    },
-    productListReducer: {
-        products: []
-    },
-    productReducer: {
-        product: { reviews: [] }
-    },
-    userRegisterReducer: {}
+  cartReducer: {
+    cartItems: [],
+    shippingAddress: {},
+  },
+  userLoginReducer: {
+    userInfo: null,
+  },
+  productListReducer: {
+    products: [],
+  },
+  productReducer: {
+    product: { reviews: [] },
+  },
+  userRegisterReducer: {},
+  
 };
 
 // Combine reducers
 const rootReducer = combineReducers({
-    productListReducer,
-    productReducer,
-    userRegisterReducer,
-    userLoginReducer,
-    cartReducer
+  productListReducer,
+  productReducer,
+  userRegisterReducer,
+  userLoginReducer,
+  cartReducer,
+  orderCreateReducer,
+  orderDetailReducer,
+  orderPaymentReducer,
+  orderListReducer,
 });
 
 // Create persisted reducer
@@ -46,9 +57,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Create store with initial state
 export const store = createStore(
-    persistedReducer,
-    initialState,
-    applyMiddleware(thunk)
+  persistedReducer,
+  initialState,
+  applyMiddleware(thunk)
 );
 
 // Create persistor
